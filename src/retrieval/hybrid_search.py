aa866c80983b50cd67c710bca2456c2ccaa5e222
Egor <kostyrineg@mail.ru>
2026-04-27 15:05:05 +0300

    feat: added Hybrid Search orchestrator for retrieval function
    - orchestrtor combines all retrieval methods together
    - semantic and index search are running async
    - returns ready-to-use chunks

---
+++ src/retrieval/hybrid_search.py
@@ -0,0 +1,69 @@
+'''Orchestrator for retrieval process'''
+
+import asyncio
+from typing import List, Tuple
+
+from indexing.vector_store import ChromaStore
+from retrieval.semantic_search import SemanticSearch
+from retrieval.bm25_search import BM25Searcher
+from retrieval.rrf import rrf_fusion
+from core.config import config
+
+class HybridSearch:
+    '''Get final chunks base for each query'''
+
+    def __init__(self):
+        self.vector_store = ChromaStore()
+        self.semantic_search = SemanticSearch()
+        self.bm25_search = BM25Searcher()
+        self.final_limit = config.retrieval.final_limit
+
+    async def search(self, query: str) -> List[dict]:
+        semantic_results, bm25_results = await asyncio.gather(
+            asyncio.to_thread(self.semantic_search.search, query),
+            asyncio.to_thread(self.bm25_search.search, query),
+    )
+
+        final_ids = rrf_fusion(semantic_results, bm25_results)
+        final_ids = final_ids[:self.final_limit]
+
+        chunks = self.vector_store.get_by_ids(final_ids)
+
+        #Parental Retrieval
+        unique_chunks = self._resolve_parents(chunks)
+        
+        return unique_chunks
+    
+    def _resolve_parents(self, chunks: List[dict]) -> List[dict]:
+        '''
+        If chunk is child and it has a parent_id -> replace it with parent chunk.
+        Delete parent duplicates.
+
+        Returns:
+            unique ready-to-use chunks for asnwer.
+        '''
+
+        seen_parents = set()
+        resolved = []
+
+        for chunk in chunks:
+            parent_id = chunk["metadata"].get("parent_id")
+
+            if parent_id:
+                if parent_id not in seen_parents:
+                    parent_chunk = self.vector_store.get_by_ids([parent_id])
+                    if parent_chunk:
+                        resolved.append(parent_chunk[0])
+                        seen_parents.add(parent_id)
+
+            else:
+                if chunk["id"] not in seen_parents:
+                    resolved.append(chunk)
+                    seen_parents.add(chunk["id"])
+        
+        return resolved
+
+
+
+
+    
