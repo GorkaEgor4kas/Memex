8e6a32860415a9e936dc22fab44608bc2ab3db00
Egor <kostyrineg@mail.ru>
2026-06-05 21:33:30 +0300

    fix: fixed the bug with deleted files reindexing

--- memex/indexing/orchestrator.py
+++ memex/indexing/orchestrator.py
@@ -103,6 +103,7 @@ class IndexOrchestrator:
         for path_str in previous_paths - current_relative:
             try:
                 to_delete.append(Path(path_str))
+                Path(path_str).relative_to(self.vault_path)
             except ValueError:
                 logger.warning("Skipping file outside vault: {}", path_str)
 
